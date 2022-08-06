c29b109a414637a49a6017ef55a79aee9115d506
lordb <bladov123@gmail.com>
2022-08-06 19:05:13 +0300

    feat: add select

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,15 +1,17 @@
 <template>
   <section class="Product-list">
     <select name="list" id="" class="filter">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
+      <option value="1">По умолчанию</option>
+      <option value="1">По уменьшанию</option>
+      <option value="3">По возрастанию</option>
+      <option value="3">По наименованию</option>
     </select>
 
     <ul>
-      <li class="item">cgbcjr</li>
-      <li class="item">cgbcjr2</li>
-      <li class="item">cgbcjr3</li>
+      <li class="item">По умолчанию</li>
+      <li class="item">По уменьшанию</li>
+      <li class="item">По возрастанию</li>
+      <li class="item">По наименованию</li>
     </ul>
   </section>
 </template>
@@ -19,6 +21,8 @@ export default {};
 </script>
 
 <style scoped lang="scss">
+@import '@/scss/variables';
+
 .Product-list {
   margin: 0;
   padding-top: 32px;
@@ -29,14 +33,36 @@ export default {};
 }
 
 .filter {
-  margin: 0;
+  margin: 0 0 16px 0;
   justify-self: end;
   align-self: end;
+  padding: 10px 0 10px 16px;
+
+  font-size: 12px;
+  line-height: 15px;
+  color: $grey-03;
+
+  background: $white-01;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border: none;
+  border-radius: 4px;
+
+  &:hover {
+    box-shadow: 0px 2px 5px rgba(5, 163, 236, 0.986);
+  }
+
+  &:focus {
+    outline: 1px solid rgba(5, 163, 236, 0.986);
+  }
 }
 
 ul {
   margin: 0;
   list-style-type: none;
+}
+
+li {
+  margin: 0;
 }
 
 .item {
